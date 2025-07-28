82d7f46918fc799c7b2a2a29ea4e0b8a60b20ae2
hrdbread <stefan@twerdochlib.com>
2025-07-28 09:35:42 +0200

    Add files via upload

---
+++ config.rb
@@ -0,0 +1,9 @@
+
+css_dir = "assets/css" # by Compass.app 
+sass_dir = "styling" # by Compass.app 
+images_dir = "images" # by Compass.app 
+output_style = :compressed # by Compass.app 
+relative_assets = true # by Compass.app 
+line_comments = true # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = false # by Compass.app 
